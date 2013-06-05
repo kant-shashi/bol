0893d0b7d03566cb1820e5f2b0421507c831288e
Shashi Kant <supershashikant@gmail.com>
2013-06-05 19:05:23 +0530

    add few more modification

--- test.py
+++ test.py
@@ -3,35 +3,47 @@
 from couchbase import Couchbase
 from pprint import pprint
 
-cb=Couchbase.connect(bucket="beer-sample")
-result = cb.get('new_holland_brewing_company-sundog')
+# cb=Couchbase.connect(bucket="beer-sample")
+# result = cb.get('new_holland_brewing_company-sundog')
 
-pprint(result.value,indent=4)
+# pprint(result.value,indent=4)
 
-new_beer = {
-    'name': 'Old yankee ale',
-    'abv':5.00,
-    'srm':0,
-    'upc':0,
-    'type':'beer',
-    'brewery_id':'cottrell_brewing_co',
-    'updated':'2012-08-o30',
-    'description':'.A medium_bodied Amber Ale',
-    'style':'American-Style Amber',
-    'category':'North American Ale'
-}
+# new_beer = {
+#     'name': 'Old yankee ale',
+#     'abv':5.00,
+#     'srm':0,
+#     'upc':0,
+#     'type':'beer',
+#     'brewery_id':'cottrell_brewing_co',
+#     'updated':'2012-08-o30',
+#     'description':'.A medium_bodied Amber Ale',
+#     'style':'American-Style Amber',
+#     'category':'North American Ale'
+# }
 
 
-key = '{0}-{1}'.format(
-    new_beer['brewery_id'],
-    new_beer['name'].replace(' ','_').lower()
-)
-
-result = cb.set(key,new_beer)
-print result
-
+# key = '{0}-{1}'.format(
+#     new_beer['brewery_id'],
+#     new_beer['name'].replace(' ','_').lower()
+# )
+# result = cb.set(key,new_beer)
+# print result
 # cb.delete(key)
 
+c = Couchbase.connect(bucket='default')
+c.set('user::count',100)
+c.incr('user::count')
+c.set('foo','aaa')
+c.append('foo','bbb')
+# x=c.get('foo')
+# print x
+c.set('name','shashi kant')
+user={
+    'name':'shashi kant',
+    'roll':462,
+    'college':'nit jamshedpur'
+}
+c.set('user',user)
 
 
 
